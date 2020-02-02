ff506d6fe828b3b4c215a29a765fb7eefed8929b
y.troniak <y.troniak@gmail.com>
2020-02-02 19:21:04 +0200

    Updates

---
+++ precache-manifest.6fe9797e01c9a823b5061a1a6afc32fc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d117427b89e80ac67082b5626d713259",
+    "url": "/assets/index.html"
+  },
+  {
+    "revision": "fb0bf3817ebdfd29e896",
+    "url": "/assets/static/css/2.24bf1742.chunk.css"
+  },
+  {
+    "revision": "e2b7e7222150b6b5f250",
+    "url": "/assets/static/css/main.d878f47d.chunk.css"
+  },
+  {
+    "revision": "fb0bf3817ebdfd29e896",
+    "url": "/assets/static/js/2.11d4993d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/assets/static/js/2.11d4993d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2b7e7222150b6b5f250",
+    "url": "/assets/static/js/main.afe650bf.chunk.js"
+  },
+  {
+    "revision": "aea74c74588d8cc09f71",
+    "url": "/assets/static/js/runtime-main.e0cda0f9.js"
+  },
+  {
+    "revision": "5a670ef86c262006c3be7475e734f687",
+    "url": "/assets/static/media/icon-chevron.5a670ef8.svg"
+  },
+  {
+    "revision": "a1e6b0c561ed05e7a1d088111a8512e5",
+    "url": "/assets/static/media/icon-contractors.a1e6b0c5.svg"
+  },
+  {
+    "revision": "be066daa4d778264ec57d84d3c6566c0",
+    "url": "/assets/static/media/icon-delete.be066daa.svg"
+  },
+  {
+    "revision": "313f1818518ac4a4013d40df306fa0e7",
+    "url": "/assets/static/media/icon-dispatch.313f1818.svg"
+  },
+  {
+    "revision": "150161cbf6aa4226ba824ac10f8973ab",
+    "url": "/assets/static/media/icon-edit.150161cb.svg"
+  },
+  {
+    "revision": "fa66c852b48e3f641ec9a112e6aa7d60",
+    "url": "/assets/static/media/icon-project.fa66c852.svg"
+  },
+  {
+    "revision": "e49f4638623d9a84f66d350a738bc7da",
+    "url": "/assets/static/media/icon-properties.e49f4638.svg"
+  },
+  {
+    "revision": "f3867d5c43fe74d341e987d2b398177d",
+    "url": "/assets/static/media/icon-quotes.f3867d5c.svg"
+  },
+  {
+    "revision": "40f3a32bc6cb115dd12dd265b4bb2f7d",
+    "url": "/assets/static/media/icon-settings.40f3a32b.svg"
+  },
+  {
+    "revision": "1d769ddf5012532e23d8e64377b22ced",
+    "url": "/assets/static/media/icon-sort-bottom.1d769ddf.svg"
+  },
+  {
+    "revision": "b432452d8015ddd9defda8d33a63b016",
+    "url": "/assets/static/media/icon-sort-top.b432452d.svg"
+  },
+  {
+    "revision": "c966dbd9c747b1f5bf099365dce9c9c6",
+    "url": "/assets/static/media/icon-tracker.c966dbd9.svg"
+  },
+  {
+    "revision": "ba1e139c2fbbfa9eab334f951975a083",
+    "url": "/assets/static/media/icons-logout.ba1e139c.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/assets/static/media/logo.5d5d9eef.svg"
+  }
+]);
